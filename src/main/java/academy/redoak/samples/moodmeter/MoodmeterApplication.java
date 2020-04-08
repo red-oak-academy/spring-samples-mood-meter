dd851cf49f282782fd3def1a1a568eb34a665e9b
Dean Eckert <dean.eckert@red-oak-consulting.com>
2020-04-08 12:28:28 +0200

    intialized with start.spring.io

---
+++ src/main/java/academy/redoak/samples/moodmeter/MoodmeterApplication.java
@@ -0,0 +1,13 @@
+package academy.redoak.samples.moodmeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodmeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodmeterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/academy/redoak/samples/moodmeter/MoodmeterApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.redoak.samples.moodmeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodmeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
